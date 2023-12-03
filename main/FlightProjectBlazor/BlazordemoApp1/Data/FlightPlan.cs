32b32851b4ecc315a51263c863263f5f39a4b338
bebriks <114162901+bebriks@users.noreply.github.com>
2023-12-03 20:32:00 +0500

    Add files via upload

---
+++ main/FlightProjectBlazor/BlazordemoApp1/Data/FlightPlan.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazordemoApp1.Data
+{
+    public class FlightPlan
+    {
+        public string Course { get; set; }
+        public double IndicatedAirSpeed { get; set; }
+        public double TrueAltitude { get; set; }
+    }
+}
---
+++ main/FlightProjectBlazor/BlazordemoApp1/Data/FlightPlanService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazordemoApp1.Data
+{
+    public interface IFlightPlanService
+    {
+        void AddFlightPlan(FlightPlan flightPlan);
+        List<FlightPlan> GetAllFlightPlans();
+    }
+    public class FlightPlanService : IFlightPlanService
+    {
+        private List<FlightPlan> flightPlans;
+
+        public FlightPlanService()
+        {
+            flightPlans = new List<FlightPlan>();
+        }
+
+        public void AddFlightPlan(FlightPlan flightPlan)
+        {
+            flightPlans.Add(flightPlan);
+        }
+
+        public List<FlightPlan> GetAllFlightPlans()
+        {
+            return flightPlans;
+        }
+    }
+}
